7e22e1c44e92dd3e592f7a802e035d1e354b42d8
erol kaftanoglu <erolkaftanoglu@gmail.com>
2016-05-20 15:44:30 +0300

    first commit

--- app/src/main/java/com/milklab34/retrofitbasicsample/MainActivity.java
+++ app/src/main/java/com/milklab34/retrofitbasicsample/MainActivity.java
@@ -2,12 +2,48 @@ package com.milklab34.retrofitbasicsample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.milklab34.retrofitbasicsample.model.GetRepo;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+    TextView repoName;
 
+    Retrofit retrofit;
+    MyApi myApi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        repoName = (TextView) findViewById(R.id.textview);
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl(MyApi.url)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        myApi  = retrofit.create(MyApi.class);
+
+        myApi.getRepo().enqueue(new Callback<GetRepo>() {
+            @Override
+            public void onResponse(Call<GetRepo> call, Response<GetRepo> response) {
+                Toast.makeText(getApplicationContext(),"Response is succes",Toast.LENGTH_SHORT).show();
+                if (response.isSuccess()){
+                    repoName.setText(response.body().getFullName());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<GetRepo> call, Throwable t) {
+                Toast.makeText(getApplicationContext(),"Response is failure", Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
--- app/src/main/java/com/milklab34/retrofitbasicsample/MyApi.java
+++ app/src/main/java/com/milklab34/retrofitbasicsample/MyApi.java
@@ -1,5 +1,10 @@
 package com.milklab34.retrofitbasicsample;
 
+import com.milklab34.retrofitbasicsample.model.GetRepo;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 /**
  * Created by erolkaftanoglu on 20/05/16.
  */
